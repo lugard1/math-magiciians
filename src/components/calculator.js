263a6be7ca4056e23bf8e89d621faf0bdda7df90
lugard1 <agulugard@gmail.com>
2023-01-16 13:42:37 +0100

    add logic functions and fix linter errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,46 +1,87 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.PureComponent {
-  render() {
-    return (
-      <div className="calculator-keypad">
-        <div className="input-keys">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="operators">
-            &divide;
-          </button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
+  constructor(props) {
+    super(props);
+    this.state = { total: 0, next: null, operation: null };
+  }
 
-          <button type="button" className="operators">
+  clickReceived = (event) => {
+    const buttonName = event.target.innerText;
+    const answer = calculate(this.state, buttonName);
+    this.setState(answer);
+  };
+
+  render() {
+    const { total, operation, next } = this.state;
+    return (
+      <div className="calculator">
+        <div className="calculator-display">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <div className="operations">
+          <button type="button" onClick={this.clickReceived}>
+            AC
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            +/-
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            %
+          </button>
+          <button type="button" onClick={this.clickReceived} className="operators">
+            &#247;
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            7
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            8
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            9
+          </button>
+          <button type="button" onClick={this.clickReceived} className="operators">
             x
           </button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="operators">
+          <button type="button" onClick={this.clickReceived}>
+            4
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            5
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            6
+          </button>
+          <button type="button" onClick={this.clickReceived} className="operators">
             -
           </button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="operators">
+          <button type="button" onClick={this.clickReceived}>
+            1
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            2
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            3
+          </button>
+          <button type="button" onClick={this.clickReceived} className="operators">
             +
           </button>
-          <button type="button" className="zerodigit">
+          <button type="button" onClick={this.clickReceived} className="zero">
             0
           </button>
-          <button type="button">.</button>
-          <button type="button" className="operators">
+          <button type="button" onClick={this.clickReceived}>
+            .
+          </button>
+          <button type="button" onClick={this.clickReceived} className="operators">
             =
           </button>
-
         </div>
       </div>
-
     );
   }
 }
