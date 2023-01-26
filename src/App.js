a79948f3a3c730ff37b2680447b39ccfaa87beb2
Lugard Agu <agulugard@gmail.com>
2023-01-26 12:13:21 +0100

    update app file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
+import Home from './components/pages/Home';
+import Quote from './components/pages/Quote';
 import './App.css';
 
 class App extends React.PureComponent {
   render() {
     return (
       <div className="application">
-        <Calculator />
+        <header>
+          <nav>
+            <h1>Math Magicians</h1>
+            <ul>
+              <li>
+                <link to="/" className="home_link" />
+                Home
+              </li>
+              <li>
+                <link to="/Calculator" className="calculator_link" />
+                Calculator
+              </li>
+              <li>
+                <link to="/Quote" className="quote_link" />
+                Quote
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Calculator" element={<Calculator />} />
+          <Route path="/Quote" element={<Quote />} />
+        </Routes>
       </div>
     );
   }
---
+++ src/components/pages/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="quote">
+    <p>
+      &quot;Pure mathematics is, in its way, the poetry of logical ideas.&quot; - Albert Eistein.
+    </p>
+  </div>
+);
+
+export default Quote;
